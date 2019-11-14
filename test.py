dc5702a3b46fc2ba7044559b07ff260b097c7c15
duanwenhuiIMAU <duanwenhui11.3@outlook.com>
2019-11-14 14:42:58 +0800

    all done

--- test.py
+++ test.py
@@ -1,1 +1,12 @@
-from . import WordCount
+from exam import *
+import sys
+if __name__ == '__main__':
+    if len(sys.argv) >= 2:
+        filename = sys.argv[1]
+    else:
+        print('请输入要处理的文件名：',end = ' ')
+        filename = input()
+    w = WordCount(readIn(filename))
+    w.showTotal()
+    w.showTop_low()
+    w.showHistogram()
